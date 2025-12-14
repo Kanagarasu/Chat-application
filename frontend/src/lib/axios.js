2f7409018215b302df71e792b837ee6773b39aac
Kanagarasu <kanagarasu445@gmail.com>
2025-12-14 23:31:18 +0530

    new-signup-frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance =axios.create({
-    // original
-    // baseURL:import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
-
-    // chatgpt
-    baseURL:"http://localhost:3000/api",
+    baseURL:import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
     withCredentials:true,
 });
