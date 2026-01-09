6dc8b2b8cdcb1a0324a136caa3e99ac6182cad7d
Kanagarasu <kanagarasu445@gmail.com>
2026-01-09 20:06:45 +0530

    axios url changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance =axios.create({
-    baseURL:import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chat-application-e1uy.onrender.com//api",
+    baseURL:import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chat-application-e1uy.onrender.com/api",
     withCredentials:true,
 });
